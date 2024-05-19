63670535a4d026605e181a188ad2745eb1c8a659
David Vidal <dvidalv@gmail.com>
2024-05-18 22:26:10 -0500

    Mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import logo from './assets/images/giganetLogo.png'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <div className="container">
+          <div className="logo-container">
+            <img className="logo" src={logo} alt="logo" />
+          </div>
+          <h1>Giganet Comming Soon...</h1>
+        </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
