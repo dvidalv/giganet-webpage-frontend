27acc8d3ced792827e4cd461e763297a31c3dcad
David Vidal <dvidalv@gmail.com>
2024-05-18 22:36:34 -0500

    centralizando contenido

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import logo from './assets/images/giganetLogo.png'
 import './App.css'
 
 function App() {
-  
+
 
   return (
-    <>
-      <div>
-        <div className="container">
-          <div className="logo-container">
-            <img className="logo" src={logo} alt="logo" />
-          </div>
-          <h1>Giganet Comming Soon...</h1>
-        </div>
+
+
+    <div className="container">
+      <div className="logo-container">
+        <img className="logo" src={logo} alt="logo" />
       </div>
-      
-    </>
+      <h1>Giganet Comming Soon...</h1>
+    </div>
+
+
+
   )
 }
 
