ac4ac2cc1a4943e450d226cf8407698331f77815
David Vidal <dvidalv@gmail.com>
2024-05-28 15:44:14 -0500

    refactor: Update logo and text color in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
 	return (
-		<div className="w-full bg-hero-pattern mx-auto outline outline-2 outline-red-500 h-screen flex justify-center items-center">
+		<div className="w-full mx-auto outline outline-2 outline-red-500 h-screen flex justify-center items-center">
 			<div className="logo-container flex flex-col items-center">
 				<img src={logo} alt="logo" className='w-[100px] h-[100px] mb-4' />
-				<h1 className='text-white text-4xl font-bold text-center '>GigaNet muy pronto...</h1>
+				<h1 className='text-orange-500 text-4xl font-bold text-center mt-5 '>GigaNet muy pronto...</h1>
 			</div>
 		</div>
 	);
