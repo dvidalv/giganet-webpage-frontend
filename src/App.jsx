7d4c566ccc0c1fd7c3b0a5574cf787e10b57a7c8
David Vidal <dvidalv@gmail.com>
2024-05-28 15:28:42 -0500

    refactor: Update background image and logo in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
 	return (
-		<div className="w-full bg-black mx-auto outline outline-2 outline-red-500 h-screen flex justify-center items-center">
+		<div className="w-full bg-hero-pattern mx-auto outline outline-2 outline-red-500 h-screen flex justify-center items-center">
 			<div className="logo-container flex flex-col items-center">
 				<img src={logo} alt="logo" className='w-[100px] h-[100px] mb-4' />
 				<h1 className='text-white text-4xl font-bold text-center '>GigaNet muy pronto...</h1>
