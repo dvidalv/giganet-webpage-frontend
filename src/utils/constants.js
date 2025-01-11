0f9e58a7fd0920f4fb448f72a84b596ef5d243db
David Vidal <dvidalv@gmail.com>
2025-01-11 16:13:26 -0400

    update main url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,22 +1,10 @@
 let SERVER_URL = '';
 
 if (window.location.hostname === 'localhost') {
-	SERVER_URL = 'http://localhost:3001';
+	SERVER_URL = 'http://localhost:3000';
 } else {
 	SERVER_URL = 'https://giganet-backend.vercel.app';
 }
 
-export const menuLinks = [
-	{ to: 'hero', text: 'Inicio', submenu: false },
-	{ to: 'servicios', text: 'Servicios', submenu: false },
-	{ to: 'nosotros', text: 'Nosotros', submenu: false },
-	{ to: 'clientes', text: 'Clientes', submenu: false },
-	{ to: 'contact', text: 'Contacto', submenu: false },
-	{ to: 'login', text: 'Login', submenu: false },
-];
-
-// export const FILEMAKER_URL = 'https://www.server-lpcr.com.doo';
-// export const FILEMAKER_DATABASE = 'lpcr';
-// export const FILEMAKER_LAYOUT = 'pacientes_web';
 
 export default SERVER_URL;
